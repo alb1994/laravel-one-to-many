7582ccc18f7c55c3de578ac63b3a4092eb31a09f
alb1994 <antonetti_alberto@libero.it>
2023-08-29 15:56:30 +0200

    aggiorno show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,17 +2,22 @@
 
 @section('content')
 <div class="container mt-5">
-    <div class="row">
-        <div class="col-md-8 offset-md-2">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
                     <h1 class="h3">{{ $post->title }}</h1>
                 </div>
                 <div class="card-body">
-                    <img src="{{ asset('storage/'. $post->cover_image) }}" alt="Immagine di copertina" class="img-fluid mb-3">
+                    <img src="{{ asset($post->cover_image) }}" alt="Cover Image" class="img-fluid mb-3">
                     <p class="lead">{{ $post->content }}</p>
                     <div class="mt-4">
-                        <strong>Categoria:</strong> {{ $post->category->name }}
+                        <strong>Categoria:</strong> 
+                        @if($post->category)
+                            {{ $post->category->name }}
+                        @else
+                            Senza Categoria
+                        @endif
                     </div>
                 </div>
             </div>
